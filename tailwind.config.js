d60c3f368237525ea4ebbdf665ac2e2796b2125d
alihoushngi <blumy.info@fmail.com>
2024-05-12 00:41:07 +0330

    colors config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1c252e",
+        secondary: "#263240",
+        playerColor: "f5ba22",
+        textColor: "cdcbca",
+        warnColor: "fd5749",
+      },
+    },
   },
   plugins: [],
 };
