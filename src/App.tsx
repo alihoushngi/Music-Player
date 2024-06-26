50ef3f5618ffb15ea3e93285113a07e7d9cfb9f1
alihoushngi <blumy.info@fmail.com>
2024-06-26 15:38:01 +0330

    welcome section done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import DesktopLayout from "./components/Base/DesktopLayout/DesktopLayout";
+import WelcomeSection from "./components/Shared/WelcomeSection/WelcomeSection";
 
 function App() {
   return (
     <div className="flex justify-between min-h-screen">
       <DesktopLayout>
-        <main className="w-2/4"></main>
+        <main className="w-2/4 px-5 py-9">
+          <WelcomeSection />
+        </main>
       </DesktopLayout>
     </div>
   );
---
+++ src/components/Shared/WelcomeSection/WelcomeSection.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+const WelcomeSection = () => {
+  const [time, setTime] = useState(new Date());
+  const [welcomeMessage, setWelcomeMessage] = useState("");
+
+  useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerID);
+  }, []);
+
+  useEffect(() => {
+    const hour = time.getHours();
+    if (hour >= 0 && hour < 10) {
+      setWelcomeMessage("Good morning");
+    } else if (hour >= 10 && hour < 16) {
+      setWelcomeMessage("Good evening");
+    } else {
+      setWelcomeMessage("Good night");
+    }
+  }, [time]);
+
+  const tick = () => {
+    setTime(new Date());
+  };
+
+  return (
+    <section className="flex justify-between items-center">
+      <div className="flex flex-col">
+        <span className="font-medium text-3xl">{welcomeMessage}</span>
+        <span className="font-normal text-md">Ali Houshangi</span>
+      </div>
+      <span className="bg-playerColor px-6 rounded-md text-primary">
+        {time.toLocaleTimeString()}
+      </span>
+    </section>
+  );
+};
+
+export default WelcomeSection;
